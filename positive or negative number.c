7e564fbee9f05f56239fefd76d69b8e200eb0b84
Veera Siva Naga Jyothi Tekumudi <117423475+JyothiTekumudi@users.noreply.github.com>
2023-06-05 05:41:13 -0700

    Add files via upload

---
+++ positive or negative number.c
@@ -0,0 +1,16 @@
+//positive or negative number
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the number");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("positive number");
+	}
+	else
+	{
+		printf("negative number");
+	}
+}
